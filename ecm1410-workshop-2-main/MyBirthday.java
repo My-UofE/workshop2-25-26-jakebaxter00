352e0e05a77bf716d5a40a644da6067acbd4c6eb
jakebaxter00 <jb1860@exeter.ac.uk>
2026-01-28 23:30:27 +0000

    my birthday - 3.2

--- ecm1410-workshop-2-main/MyBirthday.java
+++ ecm1410-workshop-2-main/MyBirthday.java
@@ -12,13 +12,16 @@ public class MyBirthday {
 
         // 1. calc number of days from current date
         //    to birthday 
+        int daysLeftToBirthday = myBirthdayDate - currentDate;
 
 
         // 2. use this along with the current weekday 
         //    to find when the birthday falls
+        int dayOfBirthday = (currentDay + daysLeftToBirthday) % DAYS_A_WEEK;
 
 
         // 3. Display the result
+        System.out.println("The day of the birthday is: " + dayOfBirthday);
     
 
     }
