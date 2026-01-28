6048d21124ac9fdb26ae709caf733b392dfac278
jakebaxter00 <jb1860@exeter.ac.uk>
2026-01-28 05:30:27 +0000

    q

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -1,3 +1,11 @@
 // Add your code below
-
-
+// HelloWorld application
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.print("Hello World! My name is ");
+        System.out.print(args[0]);
+        System.out.print(". I am ");
+        System.out.print(args[1]);
+        System.out.println(" years old.");
+    }
+}
---
+++ ecm1410-workshop-2-main/MyBirthday.java
@@ -0,0 +1,25 @@
+// MyBirthday application
+public class MyBirthday {
+    public static void main(String[] args) {
+        int currentDay = 2; 
+        int currentDate = 7; 
+        int myBirthdayDate = 23; 
+
+        // value below is a constant
+        final int DAYS_A_WEEK = 7; 
+        
+        // Tasks
+
+        // 1. calc number of days from current date
+        //    to birthday 
+
+
+        // 2. use this along with the current weekday 
+        //    to find when the birthday falls
+
+
+        // 3. Display the result
+    
+
+    }
+}
